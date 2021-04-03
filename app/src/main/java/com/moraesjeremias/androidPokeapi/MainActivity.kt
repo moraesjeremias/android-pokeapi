d1824a16756216d9c1d8f21ac46506b016eca18c
Jeremias Moraes <jeremias@volanty.com>
2021-04-03 16:44:03 -0300

    Updating readme

--- app/src/main/java/com/moraesjeremias/androidPokeapi/MainActivity.kt
+++ app/src/main/java/com/moraesjeremias/androidPokeapi/MainActivity.kt
@@ -2,10 +2,21 @@ package com.moraesjeremias.androidPokeapi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.moraesjeremias.androidPokeapi.data.api.PokeApi
+import com.moraesjeremias.androidPokeapi.databinding.ActivityMainBinding
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        val pokeApi = PokeApi()
+        runBlocking {
+            pokeApi.getPokemonList(offset = 1)
+        }
     }
+
+
 }
