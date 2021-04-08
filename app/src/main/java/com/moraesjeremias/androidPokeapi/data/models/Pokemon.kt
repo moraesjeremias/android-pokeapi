86411a5a5069719ea5f1f5aa401498bc31f3c482
Jeremias Moraes <jeremias@volanty.com>
2021-04-07 22:08:46 -0300

    Creating api model and changing pokemon domain

---
+++ app/src/main/java/com/moraesjeremias/androidPokeapi/data/models/Pokemon.kt
@@ -0,0 +1,40 @@
+package com.moraesjeremias.androidPokeapi.data.models
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Pokemon(
+    val id: String,
+    val name: String,
+    val sprites: SpriteType,
+    val types: List<PokemonType>
+)
+
+@Serializable
+data class PokemonType(
+    val type: PokemonTypeName
+)
+
+@Serializable
+data class PokemonTypeName(
+    val name: String
+)
+
+@Serializable
+data class SpriteType(
+    @SerialName("other")
+    val spriteType: PokemonSpriteType
+)
+
+@Serializable
+data class PokemonSpriteType(
+    @SerialName("dream_world")
+    val artwork: PokemonArtwork
+)
+
+@Serializable
+data class PokemonArtwork(
+    @SerialName("front_default")
+    val imageURL: String
+)
--- app/src/main/java/com/moraesjeremias/androidPokeapi/domain/Pokemon.kt
+++ app/src/main/java/com/moraesjeremias/androidPokeapi/domain/Pokemon.kt
@@ -1,30 +1,44 @@
 package com.moraesjeremias.androidPokeapi.domain
 
-import kotlinx.serialization.SerialName
-import kotlinx.serialization.Serializable
+import androidx.compose.ui.graphics.Color
 
 
-@Serializable
 data class Pokemon(
     val id: String,
     val name: String,
-    val sprites: SpriteType
+    val type: List<PokemonTypeEnum>,
+    val artWork: String
 )
 
-@Serializable
-data class SpriteType(
-    @SerialName("other")
-    val spriteType: PokemonSpriteType
-)
 
-@Serializable
-data class PokemonSpriteType(
-    @SerialName("dream_world")
-    val artwork: PokemonArtwork
-)
+enum class PokemonTypeEnum(val color: Color, val inverseColor: Color, val description: String) {
+    GRASS(color = Color.Green, inverseColor=Color.White, description = "grass"),
+    POISON(color = Color.Green, inverseColor=Color.White, description = "poison"),
+    FIRE(color = Color.Red, inverseColor=Color.White, description = "fire"),
+    FLYING(color = Color.Cyan, inverseColor=Color.White, description = "flying"),
+    WATER(color = Color.Blue, inverseColor=Color.White, description = "water"),
+    BUG(color = Color.LightGray, inverseColor=Color.White, description = "bug"),
+    NORMAL(color = Color.White, inverseColor=Color.White, description = "normal"),
+    ELECTRIC(color = Color.Yellow, inverseColor=Color.Black, description = "electric"),
+    GROUND(color = Color.DarkGray, inverseColor=Color.White, description = "ground"),
+    FAIRY(color = Color.White, inverseColor=Color.White, description = "fairy"),
+    FIGHTING(color = Color.Gray, inverseColor=Color.White, description = "fighting"),
+    PSYCHIC(color = Color.Magenta, inverseColor=Color.White, description = "psychic"),
+    ROCK(color = Color.White, inverseColor=Color.White, description = "rock"),
+    STEEL(color = Color.Gray, inverseColor=Color.White, description = "steel"),
+    ICE(color = Color.Cyan, inverseColor=Color.White, description = "ice"),
+    GHOST(color = Color.Black, inverseColor=Color.White, description = "ghost"),
+    DRAGON(color = Color.White, inverseColor=Color.White, description = "dragon"),
+    DARK(color = Color.Black, inverseColor=Color.White, description = "dark");
 
-@Serializable
-data class PokemonArtwork(
-    @SerialName("front_default")
-    val imageURL: String
-)
+
+
+    companion object {
+        fun getPokemonTypeEnumByDescription(description: String): PokemonTypeEnum {
+            return values().firstOrNull { pokemonTypeEnum ->
+                description.equals(pokemonTypeEnum.description, ignoreCase = true)
+            } ?: error("Enum não mapeado")
+        }
+    }
+
+}
