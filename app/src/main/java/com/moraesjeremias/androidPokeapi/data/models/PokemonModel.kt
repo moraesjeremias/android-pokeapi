dcff519c05d90d179903713bf5edb11c99ea8ad3
Jeremias Moraes <jeremias@volanty.com>
2021-04-08 00:49:20 -0300

    Parse Model to Domain

--- app/src/main/java/com/moraesjeremias/androidPokeapi/data/models/PokemonModel.kt
+++ app/src/main/java/com/moraesjeremias/androidPokeapi/data/models/PokemonModel.kt
@@ -4,7 +4,7 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class Pokemon(
+data class PokemonModel(
     val id: String,
     val name: String,
     val sprites: SpriteType,
--- app/src/main/java/com/moraesjeremias/androidPokeapi/domain/Pokemon.kt
+++ app/src/main/java/com/moraesjeremias/androidPokeapi/domain/Pokemon.kt
@@ -1,6 +1,8 @@
 package com.moraesjeremias.androidPokeapi.domain
 
 import androidx.compose.ui.graphics.Color
+import com.moraesjeremias.androidPokeapi.R
+import com.moraesjeremias.androidPokeapi.data.models.PokemonModel
 
 
 data class Pokemon(
@@ -9,6 +11,22 @@ data class Pokemon(
     val type: List<PokemonTypeEnum>,
     val artWork: String
 )
+
+fun transformModelToDomain(pokemonModel: PokemonModel) =
+    with(pokemonModel){
+        Pokemon(
+            id = id,
+            name = name,
+            type = getTypesList(pokemonModel),
+            artWork = sprites.spriteType.artwork.imageURL
+        )
+    }
+
+fun getTypesList(pokemonModel: PokemonModel): List<PokemonTypeEnum> =
+     pokemonModel.types.map { types ->
+        PokemonTypeEnum.getPokemonTypeEnumByDescription(types.type.name)
+    }
+
 
 
 enum class PokemonTypeEnum(val color: Color, val inverseColor: Color, val description: String) {
@@ -24,10 +42,10 @@ enum class PokemonTypeEnum(val color: Color, val inverseColor: Color, val descri
     FAIRY(color = Color.White, inverseColor=Color.White, description = "fairy"),
     FIGHTING(color = Color.Gray, inverseColor=Color.White, description = "fighting"),
     PSYCHIC(color = Color.Magenta, inverseColor=Color.White, description = "psychic"),
-    ROCK(color = Color.White, inverseColor=Color.White, description = "rock"),
+    ROCK(color = Color(R.color.purple_500), inverseColor= Color.White, description = "rock"),
     STEEL(color = Color.Gray, inverseColor=Color.White, description = "steel"),
     ICE(color = Color.Cyan, inverseColor=Color.White, description = "ice"),
-    GHOST(color = Color.Black, inverseColor=Color.White, description = "ghost"),
+    GHOST(color = Color(R.color.purple_500), inverseColor=Color.White, description = "ghost"),
     DRAGON(color = Color.White, inverseColor=Color.White, description = "dragon"),
     DARK(color = Color.Black, inverseColor=Color.White, description = "dark");
 
